e8a561b3d162f26fc143e510157a9223a0b821b6
frumipasztor <frumipasztor@gmail.com>
2021-08-29 22:47:49 +0200

    git build

--- frontend/src/components/BlogComponent/BlogPostMini.jsx
+++ frontend/src/components/BlogComponent/BlogPostMini.jsx
@@ -5,7 +5,7 @@ const BlogPostMini = ({ data }) => {
   return (
     <div className="BlogMini">
       <h2>{data.title}</h2>
-      <img src={`/img/${data.img}.jpg`} alt="hawaii"></img>
+      <img src={`../img/${data.img}.jpg`} alt="hawaii"></img>
       <p>{data.summary}</p>
       <Link to={data.path} className="bloglink">
         Tovább olvasok...
